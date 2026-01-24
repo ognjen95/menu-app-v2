b5a2db2efda4241d152c222c5ee4394f8c63a5c3
Ognjen Atlagic <36098636+ognjen95@users.noreply.github.com>
2026-01-25 00:06:52 +0100

    fix

--- utils/urls.ts
+++ utils/urls.ts
@@ -10,13 +10,13 @@ export const getWebsiteUrl = (website?: any): string | null => {
   const isLocalDev = process.env.NODE_ENV === 'development'
   const isStaging = typeof window !== 'undefined' && window.location.hostname.includes('vercel.app')
 
-  if (isLocalDev) {
-    return `http://localhost:3000/site/${website.subdomain}`
-  }
-
   if (isStaging) {
     return `${window.location.origin}/site/${website.subdomain}`
   }
 
+  if (isLocalDev) {
+    return `http://localhost:3000/site/${website.subdomain}`
+  }
+  
   return `https://${website.subdomain}.qrmenu.app`
 }
