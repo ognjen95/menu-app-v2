c32396e2fb654253106b9f2a18d7d934664538f5
Ognjen Atlagic <36098636+ognjen95@users.noreply.github.com>
2026-01-31 22:51:02 +0100

    feat: fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-const url = process.env.NEXT_PUBLIC_LANDING_PAGE || 'https://klopay-app-landing.webflow.io/about';
+const url = process.env.NEXT_PUBLIC_LANDING_PAGE || 'https://klopay-app-landing.webflow.io';
 
 export default function LandingPage() {
   redirect(url)
