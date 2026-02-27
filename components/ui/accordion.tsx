4f1c37fb0f9291abed1c2a8d206e0138c07bf9af
Ognjen Atlagic <36098636+ognjen95@users.noreply.github.com>
2026-02-27 13:40:50 +0100

    feat: push

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -5,6 +5,7 @@ import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDown } from "lucide-react"
 
 import { cn } from "@/lib/utils"
+import { Button } from "./button"
 
 const Accordion = AccordionPrimitive.Root
 
@@ -28,13 +29,15 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between p-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
     >
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      <Button variant="ghost" className="h-4 w-4 p-4">
+        <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      </Button>
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
